59636ff6fefa827b774a807625a2f7919709d539
Alan Jenkins <alan.jenkins@claranet.uk>
2017-04-30 22:54:54 +0100

    Add i3exit to i3_desktop profile.

--- modules/profile/manifests/i3_desktop.pp
+++ modules/profile/manifests/i3_desktop.pp
@@ -4,4 +4,11 @@ class profile::i3_desktop (
   package {$packages:
     ensure => latest,
   }
+
+  file { 'i3exit':
+    ensure  => file,
+    path    => '/usr/local/bin/i3exit',
+    content => 'puppet:///data/i3/i3exit',
+    mode    => '0755',
+    owner   => 'root',
 }
