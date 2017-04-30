1a997b0a19d3049717139ea89d40f9ce4fbe2bcb
Alan Jenkins <alan.jenkins@claranet.uk>
2017-04-30 10:32:05 +0100

    Undo change.

--- modules/aurpkg/manifests/init.pp
+++ modules/aurpkg/manifests/init.pp
@@ -2,107 +2,102 @@ define aurpkg (
   $user = undef
 ) {
   # {{{ Install cower
-  remote_file {"$title - download cower":
+  remote_file {'/tmp/cower.tar.gz':
     ensure => present,
-    path   => '/tmp/cower.tar.gz',
     source => 'https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz',
     unless => '/usr/bin/pacman -Qi cower'
   }
 
-  exec {"$title - untar cower":
-    command => '/usr/bin/tar xvf /tmp/cower.tar.gz',
+  exec { '/usr/bin/tar xvf /tmp/cower.tar.gz':
     cwd     => '/tmp/',
     onlyif  => '/usr/bin/ls /tmp/cower.tar.gz',
     unless  => '/usr/bin/pacman -Qi cower',
-    require => Remote_file["$title - download cower"]
+    require => Remote_file['/tmp/cower.tar.gz']
   }
 
-  file { "$title - fix cower dir perms":
-    path    => '/tmp/cower',
+  file { '/tmp/cower':
     ensure  => directory,
     owner   => $user,
     mode    => '0777',
-    require => Exec["$title - untar cower"]
+    require => Exec['/usr/bin/tar xvf /tmp/cower.tar.gz']
   }
 
-  exec { "$title - makepkg cower":
+  exec { 'makepkg cower':
     command => '/usr/bin/makepkg --skippgpcheck',
     cwd     => '/tmp/cower',
     user    => $user,
     unless  => '/usr/bin/pacman -Qi cower',
-    require => File["$title - fix cower dir perms"]
+    require => File['/tmp/cower']
   }
 
-  exec { "$title - install cower":
-    command  => '/usr/bin/pacman --noconfirm -U cower*.pkg.tar.xz',
+  exec { '/usr/bin/pacman --noconfirm -U cower*.pkg.tar.xz':
     cwd      => '/tmp/cower',
     unless   => '/usr/bin/pacman -Qi cower',
     provider => shell,
     require  => Exec['makepkg cower']
   }
 
-  file { "$title - cower cleanup":
-    ensure => absent,
-    path   => '/tmp/cower'
+  exec { '/usr/bin/rm -Rf /tmp/cower':
+    onlyif => '/usr/bin/ls /tmp/cower'
   }
   # }}}
   # {{{ Install pacaur
-  # package { 'expac':
-  #   ensure => latest,
-  # }
+  package { 'expac':
+    ensure => latest,
+  }
 
-  # remote_file {'/tmp/pacaur.tar.gz':
-  #   ensure => present,
-  #   source => 'https://aur.archlinux.org/cgit/aur.git/snapshot/pacaur.tar.gz',
-  #   unless => '/usr/bin/pacman -Qi pacaur',
-  # }
+  remote_file {'/tmp/pacaur.tar.gz':
+    ensure => present,
+    source => 'https://aur.archlinux.org/cgit/aur.git/snapshot/pacaur.tar.gz',
+    unless => '/usr/bin/pacman -Qi pacaur',
+  }
 
-  # exec { '/usr/bin/tar xvf /tmp/pacaur.tar.gz':
-  #   cwd     => '/tmp/',
-  #   onlyif  => '/usr/bin/ls /tmp/pacaur.tar.gz',
-  #   unless  => '/usr/bin/pacman -Qi pacaur',
-  #   require => Remote_file['/tmp/pacaur.tar.gz']
-  # }
+  exec { '/usr/bin/tar xvf /tmp/pacaur.tar.gz':
+    cwd     => '/tmp/',
+    onlyif  => '/usr/bin/ls /tmp/pacaur.tar.gz',
+    unless  => '/usr/bin/pacman -Qi pacaur',
+    require => Remote_file['/tmp/pacaur.tar.gz']
+  }
 
-  # file { '/tmp/pacaur':
-  #   ensure  => directory,
-  #   owner   => $user,
-  #   mode    => '0777',
-  #   require => Exec['/usr/bin/tar xvf /tmp/pacaur.tar.gz']
-  # }
+  file { '/tmp/pacaur':
+    ensure  => directory,
+    owner   => $user,
+    mode    => '0777',
+    require => Exec['/usr/bin/tar xvf /tmp/pacaur.tar.gz']
+  }
 
-  # exec { 'makepkg pacaur':
-  #   command => '/usr/bin/makepkg',
-  #   cwd     => '/tmp/pacaur',
-  #   user    => $user,
-  #   unless  => '/usr/bin/pacman -Qi pacaur',
-  #   require => [
-  #     File['/tmp/pacaur'],
-  #     Package['expac']
-  #   ]
-  # }
+  exec { 'makepkg pacaur':
+    command => '/usr/bin/makepkg',
+    cwd     => '/tmp/pacaur',
+    user    => $user,
+    unless  => '/usr/bin/pacman -Qi pacaur',
+    require => [
+      File['/tmp/pacaur'],
+      Package['expac']
+    ]
+  }
 
-  # exec { '/usr/bin/pacman --noconfirm -U pacaur*.pkg.tar.xz':
-  #   cwd      => '/tmp/pacaur',
-  #   unless   => '/usr/bin/pacman -Qi pacaur',
-  #   provider => shell,
-  #   require  => Exec['makepkg pacaur']
-  # }
+  exec { '/usr/bin/pacman --noconfirm -U pacaur*.pkg.tar.xz':
+    cwd      => '/tmp/pacaur',
+    unless   => '/usr/bin/pacman -Qi pacaur',
+    provider => shell,
+    require  => Exec['makepkg pacaur']
+  }
 
-  # exec { '/usr/bin/rm -Rf /tmp/pacaur':
-  #   onlyif => '/usr/bin/ls /tmp/pacaur'
-  # }
-  # # }}}
-  # # {{{ if package not installed
-  # exec { "/usr/bin/cower --d -d $title":
-  #   cwd         => "/home/$user",
-  #   provider    => shell,
-  #   environment => [
-  #     'HOME=/home/alan',
-  #   ],
-  #   user        => $user,
-  # }
-  # # }}}
+  exec { '/usr/bin/rm -Rf /tmp/pacaur':
+    onlyif => '/usr/bin/ls /tmp/pacaur'
+  }
+  # }}}
+  # {{{ if package not installed
+  exec { "/usr/bin/cower --d -d $title":
+    cwd         => "/home/$user",
+    provider    => shell,
+    environment => [
+      'HOME=/home/alan',
+    ],
+    user        => $user,
+  }
+  # }}}
   # {{{ if package already installed
   ## Get current installed version
   ## Get latest version
