eff5e6b905f128eba9dfb3efef57e86b173ecd8a
Alan Jenkins <alan.jenkins@claranet.uk>
2017-05-06 08:14:29 +0100

    Working puppet module to install XPS13 bluetooth firmware.

---
+++ modules/data/files/xps13/XPS13-BLUETOOTH-FIRMWARE.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+WINDOWS_DRIVER_URL="$1"
+TMPDIR="$(mktemp -d)"
+
+WIN_DRIVER_FILE="${TMPDIR}bluetooth_win_driver.exe"
+HEX_FIRMWARE_PATH="${TMPDIR}Win32/BCM4350C5_003.006.007.0095.1703.hex"
+HCD_FIRMWARE_PATH="/lib/firmware/brcm/BCM-0a5c-6412.hcd"
+
+cd "$TMPDIR"
+
+curl "$WINDOWS_DRIVER_URL" -o "$WIN_DRIVER_FILE"
+7z x "$WIN_DRIVER_FILE"
+hex2hcd "$HEX_FIRMWARE_PATH" -o "$HCD_FIRMWARE_PATH"
+rm -Rf "$TMPDIR"
---
+++ modules/profile/manifests/xps13bluetooth.pp
@@ -0,0 +1,25 @@
+class profile::xps13bluetooth (
+  $windows_driver_url = 'http://downloads.dell.com/FOLDER03272920M/1/9350_Network_Driver_XMJK7_WN32_12.0.1.720_A00.EXE',
+  $firmware_path = '/lib/firmware/brcm/BCM-0a5c-6412.hcd',
+  $script_path = '/usr/local/bin/xps13-bluetooth-firmware'
+)
+{
+  package { 'p7zip':
+    ensure => installed,
+  }
+
+  package { 'bluez-utils':
+    ensure => installed,
+  }
+
+  file { 'install bluetooth firmware script':
+    source  => "puppet:///modules/data/xps13/XPS13-BLUETOOTH-FIRMWARE.sh",
+    mode    => '0700',
+    path    => $script_path,
+  }
+
+  exec { 'run xps13 firmware installation script':
+    command => "$script_path $windows_driver_url",
+    creates => $firmware_path,
+  }
+}
