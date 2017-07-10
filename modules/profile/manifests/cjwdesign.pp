1f4cd5d7c4882d77b2ff3ca12b603e504e5f50d3
Alan Jenkins <alan.jenkins@claranet.uk>
2017-07-10 05:41:56 +0100

    Try comment the lot.

--- modules/profile/manifests/cjwdesign.pp
+++ modules/profile/manifests/cjwdesign.pp
@@ -3,36 +3,36 @@ class profile::cjwdesign (
   require profile::php56
   require profile::nginx
 
-  file {'cjwdesign nginx config':
-    ensure  => file,
-    path    => '/etc/nginx/sites-available/cjwdesign.net.conf',
-    content => epp('data/cjwdesign.net/nginx.conf.epp'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0664',
-    notify  => Service['nginx'],
-    require => [
-      Service['nginx'],
-      File['/etc/nginx/sites-available'],
-    ]
-  }
+  # file {'cjwdesign nginx config':
+  #   ensure  => file,
+  #   path    => '/etc/nginx/sites-available/cjwdesign.net.conf',
+  #   content => epp('data/cjwdesign.net/nginx.conf.epp'),
+  #   owner   => 'root',
+  #   group   => 'root',
+  #   mode    => '0664',
+  #   notify  => Service['nginx'],
+  #   require => [
+  #     Service['nginx'],
+  #     File['/etc/nginx/sites-available'],
+  #   ]
+  # }
 
-  file {'enable cjwdesign nginx config':
-    ensure  => link,
-    path    => '/etc/nginx/sites-enabled/cjwdesign.net.conf',
-    source  => '/etc/nginx/sites-available/cjwdesign.net.conf',
-    require => [
-      File['cjwdesign nginx config'],
-      File['/etc/nginx/sites-enabled'],
-    ],
-    notify  => Service['nginx'],
-  }
+  # file {'enable cjwdesign nginx config':
+  #   ensure  => link,
+  #   path    => '/etc/nginx/sites-enabled/cjwdesign.net.conf',
+  #   source  => '/etc/nginx/sites-available/cjwdesign.net.conf',
+  #   require => [
+  #     File['cjwdesign nginx config'],
+  #     File['/etc/nginx/sites-enabled'],
+  #   ],
+  #   notify  => Service['nginx'],
+  # }
 
-  file { 'cjwdesign site directory':
-    ensure => directory,
-    path   => '/srv/http/cjwdesign.net',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0775',
-  }
+  # file { 'cjwdesign site directory':
+  #   ensure => directory,
+  #   path   => '/srv/http/cjwdesign.net',
+  #   owner  => 'root',
+  #   group  => 'root',
+  #   mode   => '0775',
+  # }
 }
