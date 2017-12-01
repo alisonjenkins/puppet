61ffbfed39d84dfacd9ebc94175cc5929e6512a3
Alan Jenkins <alan.jenkins@claranet.uk>
2017-12-01 13:34:27 +0000

    Remove networkmanager packages from common and put in network manager profile.

--- modules/profile/manifests/networkmanager.pp
+++ modules/profile/manifests/networkmanager.pp
@@ -1,4 +1,16 @@
-class profile::networkmanager {
+class profile::networkmanager(
+  Array $packages = [
+  'network-manager-applet',
+  'networkmanager',
+  'networkmanager-openconnect',
+  'networkmanager-openvpn',
+  'networkmanager-pptp',
+  'networkmanager-vpnc',
+  ]
+){
+
+  ensure_packages($packages, { 'ensure' => 'present' })
+
   package { 'networkmanager':
     ensure => installed,
   } ->
