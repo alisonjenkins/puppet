64ea16ff941a2a6de33184d71e29fb49e6b7dff4
Alan Jenkins <alan.jenkins@claranet.uk>
2018-01-16 12:50:34 +0000

    Remove old stuff.

--- modules/profile/manifests/minecraft_sprout.pp
+++ modules/profile/manifests/minecraft_sprout.pp
@@ -25,6 +25,23 @@ class profile::minecraft_sprout (
         '/home/minecraft/sprout/usercache.json',
         '/home/minecraft/sprout/whitelist.json',
     ],
+    Array $sprout_vols = [
+        '/home/minecraft/sprout/world:/home/minecraft/world',
+        '/home/minecraft/sprout/banned-ips.json:/home/minecraft/banned-ips.json',
+        '/home/minecraft/sprout/banned-players.json:/home/minecraft/banned-players.json',
+        '/home/minecraft/sprout/logs:/home/minecraft/logs',
+        '/home/minecraft/sprout/crash-reports:/home/minecraft/crash-reports',
+        '/home/minecraft/sprout/ops.json:/home/minecraft/ops.json',
+        '/home/minecraft/sprout/usercache.json:/home/minecraft/usercache.json',
+        '/home/minecraft/sprout/usernamecache.json:/home/minecraft/usernamecache.json',
+        '/home/minecraft/sprout/whitelist.json:/home/minecraft/whitelist.json',
+        '/home/minecraft/sprout/server.properties:/home/minecraft/server.properties',
+        '/home/minecraft/sprout/journeymap:/home/minecraft/journeymap',
+        '/home/minecraft/sprout/TooManyGravesData:/home/minecraft/TooManyGravesData',
+        '/home/minecraft/sprout/backups:/home/minecraft/backups',
+        '/home/minecraft/sprout/mods.override:/home/minecraft/mods.override',
+        '/home/minecraft/sprout/config.override:/home/minecraft/config.override',
+    ],
     String $world_path = '/home/minecraft/sprout/world',
     String $container_name = 'sprout',
     String $image_name = 'sprout',
@@ -72,22 +89,6 @@ class profile::minecraft_sprout (
         ]
     }
 
-    $sprout_vols = [
-        '/home/minecraft/sprout/world:/home/minecraft/world',
-        '/home/minecraft/sprout/banned-ips.json:/home/minecraft/banned-ips.json',
-        '/home/minecraft/sprout/banned-players.json:/home/minecraft/banned-players.json',
-        '/home/minecraft/sprout/logs:/home/minecraft/logs',
-        '/home/minecraft/sprout/crash-reports:/home/minecraft/crash-reports',
-        '/home/minecraft/sprout/ops.json:/home/minecraft/ops.json',
-        '/home/minecraft/sprout/usercache.json:/home/minecraft/usercache.json',
-        '/home/minecraft/sprout/whitelist.json:/home/minecraft/whitelist.json',
-        '/home/minecraft/sprout/server.properties:/home/minecraft/server.properties',
-        '/home/minecraft/sprout/config/JourneyMapServer:/home/minecraft/config/JourneyMapServer',
-        '/home/minecraft/sprout/backups:/home/minecraft/backups',
-        '/home/minecraft/sprout/mods.override:/home/minecraft/mods.override',
-        '/home/minecraft/sprout/config.override:/home/minecraft/config.override',
-    ]
-
     docker::run { $container_name:
         image            => $image_name,
         ports            => [
@@ -111,36 +112,6 @@ class profile::minecraft_sprout (
         dest_key          => $aws_key,
         target            => $sprout_backup_bucket_path,
         remove_older_than => $sprout_backup_retention,
-        require           => Package[$cron_service_package],
-    }
-
-    docker::run { 'skyfactory3':
-        image            => 'demon012/minecraft-skyfactory3',
-        ports            => [
-            '25566:25565',
-        ],
-        expose           => [
-            '25565/tcp',
-        ],
-        env              => [
-            "MCUID=${minecraft_uid}",
-            "MCGID=${minecraft_gid}",
-            'MCMEM=4000',
-        ],
-        volumes          => $skyfactory3_vols,
-        memory_limit     => '4096m', # (format: '<number><unit>', where unit = b, k, m or g)
-        dns              => ['8.8.8.8', '8.8.4.4'],
-        restart_service  => true,
-        pull_on_start    => true,
-        extra_parameters => ['--restart=always'],
-    }
-
-    duplicity { 'skyfactory3_backup':
-        directory         => $skyfactory_world_path,
-        dest_id           => $aws_id,
-        dest_key          => $aws_key,
-        target            => $skyfactory_backup_bucket_path,
-        remove_older_than => $skyfactory_backup_retention,
         require           => Package[$cron_service_package],
     }
 }
